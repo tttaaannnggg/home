4c3b6e79bfd111c6434929f8ff1dd722d70b03b9
Tang <eytang8@gmail.com>
2020-03-31 20:41:17 -0500

    cleaning up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          here I am, you clowns
+          here i am you{" "}
+          {getNouns([
+            "clowns",
+            "jokers",
+            "animals",
+            "jerks",
+            "sheep",
+            "doofuses",
+            "bastards",
+            "lovers"
+          ])}
         </p>
       </header>
     </div>
   );
+}
+
+function getNouns(nouns) {
+  for (let noun of nouns) {
+    if (Math.random() < 0.5) {
+      return noun;
+    }
+  }
+  return nouns[nouns.length - 1];
 }
 
 export default App;
