20b048738e8527f534358a8307d2283b252fe755
ttaanngg <ttaanngg@pop-os.localdomain>
2020-10-04 16:32:13 -0500

    linting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./reset.css";
 import "./App.css";
-import Markov from 'ez-markov';
+import Markov from "ez-markov";
 const url = window.location.href;
 
 function App() {
@@ -11,111 +11,128 @@ function App() {
   return (
     <div className="App">
       <div className="Container">
-        {BlogPost({setPost, post, setMaxPost, loaded, setLoaded})}
-        <Nav className="Nav" setPost={setPost} post={post} maxPost={maxPost} loaded={loaded} />
+        {BlogPost({ setPost, post, setMaxPost, loaded, setLoaded })}
+        <Nav
+          className="Nav"
+          setPost={setPost}
+          post={post}
+          maxPost={maxPost}
+          loaded={loaded}
+        />
       </div>
     </div>
   );
 }
 
-
-function Nav(props){
-  const {setPost, post, maxPost, loaded} = props;
-  return(
+function Nav(props) {
+  const { setPost, post, maxPost, loaded } = props;
+  return (
     <div className="Nav">
-      <button onClick={()=>{
-        //don't change state unless we've finished our last fetch
-        //otherwise we could trigger an infinite fetch loop
-        if(!loaded){
-          return
-        }
-        setPost((post)=> post-1 > 0 ? post-1:post)
-      }}> {"<-prev"} </button>
-      <span> {getNavList(post, maxPost).map((i)=><button onClick={()=>setPost(i)}>{i}</button>)} </span>
-      <button onClick={()=>{
-        if(!loaded){
-          return
-        }
-        setPost((post)=>post < maxPost ? post+1:post )}}> {"next->"} </button>
+      <button
+        onClick={() => {
+          //don't change state unless we've finished our last fetch
+          //otherwise we could trigger an infinite fetch loop
+          if (!loaded) {
+            return;
+          }
+          setPost((post) => (post - 1 > 0 ? post - 1 : post));
+        }}
+      >
+        {" "}
+        {"<-prev"}{" "}
+      </button>
+      <span>
+        {" "}
+        {getNavList(post, maxPost).map((i) => (
+          <button onClick={() => setPost(i)}>{i}</button>
+        ))}{" "}
+      </span>
+      <button
+        onClick={() => {
+          if (!loaded) {
+            return;
+          }
+          setPost((post) => (post < maxPost ? post + 1 : post));
+        }}
+      >
+        {" "}
+        {"next->"}{" "}
+      </button>
     </div>
-  )
+  );
 }
 
-function getNavList(current, max){
+function getNavList(current, max) {
   //returns array of 5 elements
   //represents pagination
   const output = [];
   let start = current - 2;
   let end = current + 2;
-  if(current+2 > max){
+  if (current + 2 > max) {
     // if  we're close to the end
     start = max - 4;
     end = max;
-  }else if(current < 3){
+  } else if (current < 3) {
     // if  we're close to the start
     start = 1;
     end = 5;
   }
-  for(let i = start; i <= end; i++){
-    if(i < 1 || i > max){
-      continue
+  for (let i = start; i <= end; i++) {
+    if (i < 1 || i > max) {
+      continue;
     }
-    if(i === current){
+    if (i === current) {
       output.push(<strong>{i}</strong>);
-    }else{
+    } else {
       output.push(i);
     }
   }
   return output;
 }
 
-function BlogHeader(props){
-  const {title, created} = props.item
-  return (<div className="BlogHeader">
+function BlogHeader(props) {
+  const { title, created } = props.item;
+  return (
+    <div className="BlogHeader">
       <h1>{title}</h1>
       <em>{created}</em>
-    </div>)
+    </div>
+  );
 }
 
-function BlogPost(props){
-  const {setPost, post, setMaxPost, setLoaded} = props;
+function BlogPost(props) {
+  const { setPost, post, setMaxPost, setLoaded } = props;
   const [item, setItem] = useState({});
 
-  useEffect(
-    ()=>{
-      console.log(`fetching to ${url}api/posts/${post}`);
-      setLoaded(false)
-      fetch(`${url}api/posts/${post}`)
-        .then(res=>res.json())
-        .then(
-          result =>{
-            setLoaded(true)
-            if(result && result[0]){
-              setItem(result[0]);
-              setPost(result[0].id)
-              if(!post){
-                setMaxPost(result[0].id);
-              }
-            };
+  useEffect(() => {
+    console.log(`fetching to ${url}api/posts/${post}`);
+    setLoaded(false);
+    fetch(`${url}api/posts/${post}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setLoaded(true);
+        if (result && result[0]) {
+          setItem(result[0]);
+          setPost(result[0].id);
+          if (!post) {
+            setMaxPost(result[0].id);
           }
-        )
-    }, [props.post]
-  )
+        }
+      });
+  }, [props.post]);
 
   let content = <p>loading..</p>;
-  if(item && item.body){
-    content = item.body.split('\n').map((para, i)=>{
-      return para? (<p key={'p'+i}>{para}</p>) : <br key={'p'+i}/>
+  if (item && item.body) {
+    content = item.body.split("\n").map((para, i) => {
+      return para ? <p key={"p" + i}>{para}</p> : <br key={"p" + i} />;
     });
-  } 
-  return(
+  }
+  return (
     <>
-    <BlogHeader item ={item} />
-    <article>
-      {content}
-    </article>
+      <BlogHeader item={item} />
+      <article>{content}</article>
     </>
-  )
+  );
 }
 
 export default App;
